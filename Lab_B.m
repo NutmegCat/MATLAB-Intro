b25f0d169ba28869f95a3055f38a5ca09aa2c842
NutmegCat <deltaechogames@gmail.com>
2024-10-09 13:59:02 -0400

    first commit

---
+++ Lab_B.m
@@ -0,0 +1,3 @@
+figure(2);
+plot ([1 2 3 4], [10 20 30 40]);
+title('Plot Title - Part 3');
---
+++ Part_4.m
@@ -0,0 +1,5 @@
+figure(3);
+plot([10 20 30 40 50 60 70], [1 2 3 1 2 3 4], 'b - o', "LineWidth", 2.5);
+title("This is my title - Part 4");
+xlabel('This is the x axis');
+ylabel('This is the y axis');
